4139cab5ec6ed564d1f7be56a797952db42fd1ec
= <drahtantnayak@dal.ca>
2019-02-15 09:25:20 -0400

    Create python program which get linux command as input and perform soperate same as linux command

---
+++ command.py
@@ -0,0 +1,9 @@
+import os
+def command(cmd):
+    cmd = str(cmd)
+    fr = os.popen(cmd)
+    res = fr.read()
+    print(res)
+
+var = input("Enter linux command to perform operation:")
+command(var)
